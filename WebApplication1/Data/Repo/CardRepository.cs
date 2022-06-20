291791593554a2cffe88a9bdee8f7cfa8a7355bd
Dipesh Jadhav <Dipesh777@cqons.onmicrosoft.com>
2022-06-20 15:42:34 +0530

    Added IRepository Interface and Dtos

---
+++ WebApplication1/Data/Repo/CardRepository.cs
@@ -0,0 +1,36 @@
+﻿using CardsAPi.Data;
+using CardsAPi.Models;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Interfaces;
+
+namespace WebApplication1.Data.Repo
+{
+    public class CardRepository : ICardRepository
+    {
+        private readonly CardsDbContext dc;
+        public CardRepository(CardsDbContext dc)
+        {
+            this.dc = dc;
+        }
+        public void AddCard(Card card)
+        {
+            dc.Cards.Add(card);
+        }
+
+        public void DeleteCard(int cardId)
+        {
+            var card = dc.Cards.Find(cardId);
+            dc.Cards.Remove(card);
+        }
+
+        public async Task<Card> FindCard(int Id)
+        {
+            return await dc.Cards.FindAsync(Id);
+        }
+
+        public async Task<IEnumerable<Card>> GetCardsAsync()
+        {
+            return await dc.Cards.ToListAsync();
+        }
+    }
+}
---
+++ WebApplication1/Dtos/CardDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Dtos
+{
+    public class CardDto
+    {
+        public int Id { get; set; }
+        public string CardholderName { get; set; }
+
+        public int CardNumber { get; set; }
+    }
+}
